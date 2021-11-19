02add3f6639f827309485c17aab3bf8edb35bada
Xiaodong <71061604+Yoyo-Luming@users.noreply.github.com>
2021-11-20 03:29:03 +0800

    完成接收认为展示界面

--- wj-vue/src/components/task/receivedTask.vue
+++ wj-vue/src/components/task/receivedTask.vue
@@ -1,30 +1,121 @@
 <template>
-  <el-container>
-    <el-tabs>
-      <el-tab-pane label="待领货的任务">
-        <task-order-box v-for="(item, index) of waitGetObjectList" :key="index+Math.random()" :taskName="item.taskName" :price="item.price" :startRegion="item.startRegion" :endRegion="item.endRegion" :detail="item.detail"></task-order-box>
-      </el-tab-pane>
-      <el-tab-pane label="待送达的任务">
-        <task-order-box v-for="(item, index) of waitSendList" :key="index+Math.random()" :taskName="item.taskName" :price="item.price" :startRegion="item.startRegion" :endRegion="item.endRegion" :detail="item.detail"></task-order-box>
-      </el-tab-pane>
-      <el-tab-pane label="待收货确认的任务">
-        <task-order-box v-for="(item, index) of waitConfirmList" :key="index+Math.random()" :taskName="item.taskName" :price="item.price" :startRegion="item.startRegion" :endRegion="item.endRegion" :detail="item.detail"></task-order-box>
-      </el-tab-pane>
-      <el-tab-pane label="待评价的任务">
-        <task-order-box v-for="(item, index) of waitCommentList" :key="index+Math.random()" :taskName="item.taskName" :price="item.price" :startRegion="item.startRegion" :endRegion="item.endRegion" :detail="item.detail"></task-order-box>
-      </el-tab-pane>
-      <el-tab-pane label="已完成的任务">
-        <task-order-box v-for="(item, index) of successList" :key="index+Math.random()" :taskName="item.taskName" :price="item.price" :startRegion="item.startRegion" :endRegion="item.endRegion" :detail="item.detail"></task-order-box>
-      </el-tab-pane>
-    </el-tabs>
+  <el-container class="home-container">
+    <el-header class="el-header">
+      <div class="right-head">
+        <img src="../../assets/testlogo.png" class="logo" alt="">
+        <span class="title">SUSTech Store</span>
+      </div>
+      <div class="left-head">
+        <el-menu
+          class="el-menu-demo"
+          mode="horizontal"
+          background-color="#545c64"
+          text-color="#fff"
+          active-text-color="#ffd04b">
+          <el-menu-item index="1" v-on:click="homePage">Home Page</el-menu-item>
+          <el-menu-item index="2" v-on:click="searchPage">Search Page</el-menu-item>
+          <el-submenu index="3">
+            <template slot="title">用户名</template>
+            <el-menu-item index="3-1" v-on:click="myPage">Personal Page</el-menu-item>
+            <el-menu-item index="3-2" v-on:click="cartPage">Shopping Cart</el-menu-item>
+            <el-menu-item index="3-3" v-on:click="sellPage">Selling Page</el-menu-item>
+            <el-menu-item index="3-4" v-on:click="markPage">Marking Page</el-menu-item>
+          </el-submenu>
+          <el-menu-item index="4" v-on:click="logOut">Log Out</el-menu-item>
+        </el-menu>
+      </div>
+    </el-header>
+    <el-main>
+      <el-container class="mid-content">
+        <el-tabs type="border-card" v-model="activePane">
+          <!--          pay-pane-->
+          <el-tab-pane label="待领货" class="whole-pane" name="first">
+            <el-container class="whole-order" v-for="(item, index) in waitGetObjectList" :key="index">
+              <el-container class="order-name">{{item.taskName}}</el-container>
+              <el-container class="order-road">从{{item.startRegion}}送到{{item.endRegion}}</el-container>
+              <el-container class="order-detail">{{item.description}}</el-container>
+              <el-container class="order-price">¥{{item.price}}</el-container>
+              <el-container class="order-pay">
+                <el-button class="last-button" v-on:click="goTaskInfo(item)">查看详情</el-button>
+              </el-container>
+            </el-container>
+          </el-tab-pane>
+          <!--          send-pane-->
+          <el-tab-pane label="待送达" class="whole-pane" name="second">
+            <el-container class="whole-order" v-for="(item, index) in waitSendList" :key="index">
+              <el-container class="order-name">{{item.taskName}}</el-container>
+              <el-container class="order-road">从{{item.startRegion}}送到{{item.endRegion}}</el-container>
+              <el-container class="order-detail">{{item.description}}</el-container>
+              <el-container class="order-price">¥{{item.price}}</el-container>
+              <el-container class="order-pay">
+                <el-button class="last-button" v-on:click="goTaskInfo(item)">查看详情</el-button>
+              </el-container>
+            </el-container>
+          </el-tab-pane>
+          <!--          receive-pane-->
+          <el-tab-pane label="待收货" class="whole-pane" name="third">
+            <el-container class="whole-order" v-for="(item, index) in waitConfirmList" :key="index">
+              <el-container class="order-name">{{item.taskName}}</el-container>
+              <el-container class="order-road">从{{item.startRegion}}送到{{item.endRegion}}</el-container>
+              <el-container class="order-detail">{{item.description}}</el-container>
+              <el-container class="order-price">¥{{item.price}}</el-container>
+              <el-container class="order-pay">
+                <el-button class="last-button" v-on:click="goTaskInfo(item)">查看详情</el-button>
+              </el-container>
+            </el-container>
+          </el-tab-pane>
+          <!--          evaluate-pane-->
+          <el-tab-pane label="待评价" class="whole-pane" name="fourth">
+            <el-container class="whole-order" v-for="(item, index) in waitCommentList" :key="index">
+              <el-container class="order-name">{{item.taskName}}</el-container>
+              <el-container class="order-road">从{{item.startRegion}}送到{{item.endRegion}}</el-container>
+              <el-container class="order-detail">{{item.description}}</el-container>
+              <el-container class="order-price">¥{{item.price}}</el-container>
+              <el-container class="order-pay">
+                <el-button class="last-button" v-on:click="goTaskInfo(item)">查看详情</el-button>
+              </el-container>
+            </el-container>
+          </el-tab-pane>
+          <!--          finished-pane-->
+          <el-tab-pane label="已完成" class="whole-pane" name="fifth">
+            <el-container class="whole-order" v-for="(item, index) in successList" :key="index">
+              <el-container class="order-name">{{item.taskName}}</el-container>
+              <el-container class="order-road">从{{item.startRegion}}送到{{item.endRegion}}</el-container>
+              <el-container class="order-detail">{{item.description}}</el-container>
+              <el-container class="order-price">¥{{item.price}}</el-container>
+              <el-container class="order-pay">
+                <el-button class="last-button" v-on:click="goTaskInfo(item)">查看详情</el-button>
+              </el-container>
+            </el-container>
+          </el-tab-pane>
+        </el-tabs>
+      </el-container>
+    </el-main>
   </el-container>
+  <!--  <el-container>-->
+  <!--    <el-tabs>-->
+  <!--      <el-tab-pane label="待领货的任务">-->
+  <!--        <task-order-box v-for="(item, index) of waitGetObjectList" :key="index+Math.random()" :taskName="item.taskName" :price="item.price" :startRegion="item.startRegion" :endRegion="item.endRegion" :detail="item.detail"></task-order-box>-->
+  <!--      </el-tab-pane>-->
+  <!--      <el-tab-pane label="待送达的任务">-->
+  <!--        <task-order-box v-for="(item, index) of waitSendList" :key="index+Math.random()" :taskName="item.taskName" :price="item.price" :startRegion="item.startRegion" :endRegion="item.endRegion" :detail="item.detail"></task-order-box>-->
+  <!--      </el-tab-pane>-->
+  <!--      <el-tab-pane label="待收货确认的任务">-->
+  <!--        <task-order-box v-for="(item, index) of waitConfirmList" :key="index+Math.random()" :taskName="item.taskName" :price="item.price" :startRegion="item.startRegion" :endRegion="item.endRegion" :detail="item.detail"></task-order-box>-->
+  <!--      </el-tab-pane>-->
+  <!--      <el-tab-pane label="待评价的任务">-->
+  <!--        <task-order-box v-for="(item, index) of waitCommentList" :key="index+Math.random()" :taskName="item.taskName" :price="item.price" :startRegion="item.startRegion" :endRegion="item.endRegion" :detail="item.detail"></task-order-box>-->
+  <!--      </el-tab-pane>-->
+  <!--      <el-tab-pane label="已完成的任务">-->
+  <!--        <task-order-box v-for="(item, index) of successList" :key="index+Math.random()" :taskName="item.taskName" :price="item.price" :startRegion="item.startRegion" :endRegion="item.endRegion" :detail="item.detail"></task-order-box>-->
+  <!--      </el-tab-pane>-->
+  <!--    </el-tabs>-->
+  <!--  </el-container>-->
 </template>
 
 <script>
-import taskOrderBox from './taskOrderBox'
 export default {
   name: 'receivedTask',
-  components: {taskOrderBox},
   mounted () {
     // this.$axios.post('task/task_wait_sender_list_up/').then(response => {
     //   console.log(response.data)
@@ -40,6 +131,7 @@ export default {
           price: list[i].price,
           startRegion: this.regionList[list[i].sender_addr.user_region],
           endRegion: this.regionList[list[i].receive_addr.user_region],
+          description: list[i].description,
           detail: list[i]
         })
       }
@@ -55,6 +147,7 @@ export default {
           price: list[i].price,
           startRegion: this.regionList[list[i].sender_addr.user_region],
           endRegion: this.regionList[list[i].receive_addr.user_region],
+          description: list[i].description,
           detail: list[i]
         })
       }
@@ -70,6 +163,7 @@ export default {
           price: list[i].price,
           startRegion: this.regionList[list[i].sender_addr.user_region],
           endRegion: this.regionList[list[i].receive_addr.user_region],
+          description: list[i].description,
           detail: list[i]
         })
       }
@@ -85,6 +179,7 @@ export default {
           price: list[i].price,
           startRegion: this.regionList[list[i].sender_addr.user_region],
           endRegion: this.regionList[list[i].receive_addr.user_region],
+          description: list[i].description,
           detail: list[i]
         })
       }
@@ -100,6 +195,7 @@ export default {
           price: list[i].price,
           startRegion: this.regionList[list[i].sender_addr.user_region],
           endRegion: this.regionList[list[i].receive_addr.user_region],
+          description: list[i].description,
           detail: list[i]
         })
       }
@@ -145,10 +241,131 @@ export default {
     }
   },
   methods: {
+    goTaskInfo (item) {
+      this.$router.push({name: 'taskInfo', params: {detail: item.detail}})
+    },
+    myPage () {
+      this.$router.push('/person')
+    },
+    homePage () {
+      this.$router.push('/')
+    },
+    searchPage () {
+      this.$router.push('/search')
+    },
+    cartPage () {
+      this.$router.push('/cart')
+    },
+    sellPage () {
+      this.$router.push('/store')
+    },
+    markPage () {
+      this.$router.push('/favoritegoods')
+    },
+    logOut () {
+      this.$axios.post('login0/logout/ ')
+      this.$router.push('/login')
+    }
   }
 }
 </script>
 
 <style scoped>
+.home-container {
+  height: 100%;
+  width: 100%;
+  background: center no-repeat url("../../assets/back7.jpg");
+  background-size: cover;
+  display: block;
+}
 
+.el-header {
+  background: #545c64;
+  display: flex;
+  justify-content: space-between;
+  padding-left: 0;
+  align-items: center;
+  color: #ffffff;
+  font-size: 40px;
+  opacity: 0.5;
+}
+
+.right-head {
+  display: flex;
+  align-items: center;
+}
+
+.logo {
+  height: 60px;
+}
+
+.left-head {
+  display: flex;
+  align-items: center;
+}
+
+.mid-content {
+  display: block;
+  margin: 60px auto;
+  height: 100%;
+  width: 1200px;
+}
+
+.whole-pane {
+  display: block;
+}
+
+.whole-order {
+  display: flex;
+  justify-content: space-evenly;
+  margin-top: 5px;
+  border-bottom: 2px solid #eaeaea;
+}
+
+.order-name {
+  width: 150px;
+  overflow: hidden;
+  font-size: 30px;
+  align-items: center;
+  justify-content: space-evenly;
+  font-family: 黑体;
+}
+
+.order-road {
+  width: 150px;
+  overflow: hidden;
+  font-size: 20px;
+  align-items: center;
+  justify-content: space-evenly;
+}
+
+.order-detail {
+  width: 150px;
+  overflow: hidden;
+  font-size: 20px;
+  align-items: center;
+  justify-content: space-evenly;
+}
+
+.order-price {
+  width: 150px;
+  font-size: 20px;
+  color: #ff006e;
+  align-items: center;
+  justify-content: space-evenly;
+}
+
+.order-pay {
+  width: 150px;
+  align-items: center;
+  justify-content: space-evenly;
+}
+
+.last-button {
+  background-color: #ff0036;
+  border: 1px solid #ff0036;
+  color: #fff;
+  margin-bottom: 5px;
+  justify-content: space-evenly;
+}
 </style>

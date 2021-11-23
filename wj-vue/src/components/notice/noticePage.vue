dbabacbdb4b0085d3d4a2eaa8e97edb1a126d5e5
Xiaodong <71061604+Yoyo-Luming@users.noreply.github.com>
2021-11-23 11:43:30 +0800

    添加noticepage 顶栏

--- wj-vue/src/components/notice/noticePage.vue
+++ wj-vue/src/components/notice/noticePage.vue
@@ -3,7 +3,15 @@
     <el-header class="el-header">
       <div class="right-head">
         <img src="../../assets/testlogo.png" class="logo" alt="">
-        <span>SUSTech Store</span>
+        <span class="title">SUSTech Store</span>
+      </div>
+      <div class="mid-head">
+        <el-input
+          placeholder="请输入搜索信息"
+          prefix-icon="el-icon-search"
+          v-model="searchContent" style="width: 870px"
+          @keyup.enter.native="searchTop">
+        </el-input>
       </div>
       <div class="left-head">
         <el-menu
@@ -14,7 +22,7 @@
           active-text-color="#ffd04b">
           <el-menu-item index="1" v-on:click="homePage">主页</el-menu-item>
           <el-submenu index="2">
-            <template slot="title">{{ $store.state.userName }}</template>
+            <template slot="title">{{$store.state.userName}}</template>
             <el-menu-item index="2-1" v-on:click="myPage">个人主页</el-menu-item>
             <el-menu-item index="2-2" v-on:click="cartPage">购物车</el-menu-item>
             <el-menu-item index="2-3" v-on:click="sellPage">上架的商品</el-menu-item>
@@ -24,43 +32,91 @@
         </el-menu>
       </div>
     </el-header>
-    <el-main>
-      <el-container class="mid-content">
-        <el-tabs type="border-card" v-model="activePane">
-          <!--          pay-pane-->
-          <el-tab-pane label="消息列表" class="whole-pane" name="first">
-            <el-container class="whole-notice" v-for="(item, index) in chatList" :key="index">
-              <el-container class="message-container">
-                <div class="notice-column-user" v-if="checkUser">用户：{{item.user1Name}}</div>
-                <div class="notice-column-user" v-else>用户：{{item.user2Name}}</div>
-                <div class="notice-column-message">最后一条讯息：{{item.lastInfo}}</div>
+    <el-container style="display: flex;height: 100%;">
+      <el-aside width="200px" style="background-color: #545c64;opacity: 0.5;">
+        <el-menu
+          background-color="#545c64"
+          text-color="#fff"
+          active-text-color="#409EFF"
+          :unique-opened="true"
+          :collapse-transition="false"
+          :router="true"
+        >
+          <el-submenu class="menu-buttons" index="1">
+            <template slot="title">
+              <i class="el-icon-user"></i>
+              <span>用户信息</span>
+            </template>
+            <el-button class="inside-button" v-on:click="myPage">个人主页</el-button><br>
+            <el-button class="inside-button" v-on:click="cartPage">购物车</el-button><br>
+          </el-submenu>
+          <el-submenu class="menu-buttons" index="2">
+            <template slot="title">
+              <i class="el-icon-goods"></i>
+              <span>商品</span>
+            </template>
+            <el-button class="inside-button" v-on:click="goSellOrder">卖出的商品</el-button><br>
+            <el-button class="inside-button" v-on:click="goBuyOrder">买到的商品</el-button><br>
+            <el-button class="inside-button" v-on:click="goPostGoods">发布的商品</el-button><br>
+            <el-button class="inside-button" v-on:click="goNewGoods">上架新商品</el-button><br>
+          </el-submenu>
+          <el-submenu class="menu-buttons" index="3">
+            <template slot="title">
+              <i class="el-icon-star-off"></i>
+              <span>收藏</span>
+            </template>
+            <el-button class="inside-button" v-on:click="goFavoriteGoods">收藏的商品</el-button><br>
+            <el-button class="inside-button" v-on:click="goFavoriteUser">收藏的卖家</el-button><br>
+          </el-submenu>
+          <el-submenu class="menu-buttons" index="4">
+            <template slot="title">
+              <i class="el-icon-location-outline"></i>
+              <span>跑腿</span>
+            </template>
+            <el-button class="inside-button" v-on:click="goTaskHall">任务大厅</el-button><br>
+            <el-button class="inside-button" v-on:click="goReleasedTask">发布的跑腿任务</el-button><br>
+            <el-button class="inside-button" v-on:click="goReceivedTask">接受的跑腿任务</el-button><br>
+          </el-submenu>
+        </el-menu>
+      </el-aside>
+      <el-main style="height: 100%;padding: 0;">
+        <el-container class="mid-content">
+          <el-tabs type="border-card" v-model="activePane">
+            <!--          pay-pane-->
+            <el-tab-pane label="消息列表" class="whole-pane" name="first">
+              <el-container class="whole-notice" v-for="(item, index) in chatList" :key="index">
+                <el-container class="message-container">
+                  <div class="notice-column-user" v-if="checkUser">用户：{{item.user1Name}}</div>
+                  <div class="notice-column-user" v-else>用户：{{item.user2Name}}</div>
+                  <div class="notice-column-message">最后一条讯息：{{item.lastInfo}}</div>
+                </el-container>
+                <el-container class="notice-pay">
+                  <el-badge :value="item.waitNumber" class="item">
+                    <el-button class="last-button" type="primary" v-on:click="toChatPage(item)">查看详情</el-button>
+                  </el-badge>
+                </el-container>
               </el-container>
-              <el-container class="notice-pay">
-                <el-badge :value="item.waitNumber" class="item">
-                  <el-button class="last-button" type="primary" v-on:click="toChatPage(item)">查看详情</el-button>
+            </el-tab-pane>
+            <el-tab-pane label="通知列表" class="whole-pane" name="second">
+              <el-container class="whole-notice" v-for="(item, index) in noticeList" :key="index">
+                <el-badge is-dot :hidden="item.type === 1">
+                  <el-container class="message-container">
+                    <div class="notice-column-user" v-if="checkUser">{{item.date}}</div>
+                  </el-container>
+                  <el-container class="notice-pay">
+                    <div class="notice-column-message">{{item.information}}</div>
+
+                    <!--                <el-badge :value="item.waitNumber" class="item">-->
+                    <!--                  <el-button class="last-button" type="primary" v-on:click="toChatPage(item)">查看详情</el-button>-->
+                    <!--                </el-badge>-->
+                  </el-container>
                 </el-badge>
               </el-container>
-            </el-container>
-          </el-tab-pane>
-          <el-tab-pane label="通知列表" class="whole-pane" name="second">
-            <el-container class="whole-notice" v-for="(item, index) in noticeList" :key="index">
-              <el-badge is-dot :hidden="item.type === 1">
-                <el-container class="message-container">
-                <div class="notice-column-user" v-if="checkUser">{{item.date}}</div>
-              </el-container>
-                <el-container class="notice-pay">
-                  <div class="notice-column-message">{{item.information}}</div>
-
-<!--                <el-badge :value="item.waitNumber" class="item">-->
-<!--                  <el-button class="last-button" type="primary" v-on:click="toChatPage(item)">查看详情</el-button>-->
-<!--                </el-badge>-->
-              </el-container>
-              </el-badge>
-            </el-container>
-          </el-tab-pane>
-        </el-tabs>
-      </el-container>
-    </el-main>
+            </el-tab-pane>
+          </el-tabs>
+        </el-container>
+      </el-main>
+    </el-container>
   </el-container>
   <!--  <el-container>-->
   <!--    <notice-box v-for="(item, index) of noticeList " :key="index+Math.random()" :user1Id="item.user1Id"-->
@@ -79,7 +135,8 @@ export default {
       noticeList: [],
       start_position: 0,
       end_position: 10,
-      activePane: 'second'
+      activePane: 'second',
+      searchContent: undefined
     }
   },
   mounted () {
@@ -172,6 +229,43 @@ export default {
     },
     markPage () {
       this.$router.push('/favoritegoods')
+    },
+    goReleasedTask () {
+      this.$router.push('/releasedtask')
+    },
+    goReceivedTask () {
+      this.$router.push('/receivedtask')
+    },
+    goTaskHall () {
+      this.$router.push('/taskhall')
+    },
+    goFavoriteUser () {
+      this.$router.push('/favoriteusers')
+    },
+    goFavoriteGoods () {
+      this.$router.push('/favoritegoods')
+    },
+    goSellOrder () {
+      this.$router.push('/sellorder')
+    },
+    goBuyOrder () {
+      this.$router.push('/buyorder')
+    },
+    goPostGoods () {
+      this.$router.push('/sellinggoods')
+    },
+    goNewGoods () {
+      this.$router.push('/addgoods')
+    },
+    searchTop () {
+      this.$router.push({name: 'Result',
+        params: {
+          searchContent: this.searchContent,
+          labels: undefined,
+          status: undefined,
+          orderMethod: undefined
+        }
+      })
     }
   }
 }
@@ -181,9 +275,8 @@ export default {
 .home-container {
   height: 100%;
   width: 100%;
-  background: center no-repeat url("../../assets/back7.jpg");
+  background: center repeat url("../../assets/back7.jpg");
   background-size: cover;
-  display: block;
 }
 
 .el-header {
@@ -194,46 +287,60 @@ export default {
   align-items: center;
   color: #ffffff;
   font-size: 40px;
-  opacity: 0.5;
+  opacity: 0.7;
+}
+
+.mid-head {
+  display: flex;
+  align-items: center;
+}
+
+.menu-buttons {
+  display: block;
+  margin: auto;
+  text-align: center;
+  align-items: center;
+}
+
+.inside-button {
+  margin-top: 5px;
+  border: 0;
+  text-align: center;
+  align-items: center;
+  background-color: #545c64;
+  color: #ffffff;
 }
 
 .right-head {
   display: flex;
   align-items: center;
 }
-
 .logo {
   height: 60px;
 }
-
 .left-head {
   display: flex;
   align-items: center;
 }
-
 .mid-content {
   display: block;
   margin: 60px auto;
   height: 100%;
   width: 1200px;
 }
-
 .whole-pane {
   display: block;
 }
-
 .whole-notice {
   display: flex;
   justify-content: space-between;
   align-items: center;
   border: 1px solid #eaeaea;
 }
-
 .message-container {
   display: flex;
   text-align: center;
 }
-
 .notice-column-user {
   width: 200px;
   overflow: hidden;
@@ -241,7 +348,6 @@ export default {
   align-items: center;
   justify-content: space-evenly;
 }
-
 .notice-column-message {
   width: 700px;
   overflow: hidden;
@@ -249,7 +355,6 @@ export default {
   align-items: center;
   text-align: left;
 }
-
 .notice-pay {
   margin-top: 10px;
   margin-bottom: 10px;
@@ -257,7 +362,6 @@ export default {
   align-items: center;
   justify-content: space-evenly;
 }
-
 .last-button {
   justify-content: space-evenly;
 }
